47e2a3e3059d9f080419a58d4b6683455b5d8b7f
Maciek Dom <maciej.rudnik2@gmail.com>
2020-08-13 12:18:11 +0200

    Defining Variables - Write Some Code

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,6 @@
+var found = true;
+var grade = 66.3;
+var firstName = "Api";
+var lastName = "Gird";
+console.log(found);
+console.log("firstName" + firstName);
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,8 @@
+let found: boolean = true;
+let grade: number = 66.3;
+let firstName: string = "Api";
+let lastName: string = "Gird";
+
+
+console.log(found);
+console.log("firstName " + firstName);
